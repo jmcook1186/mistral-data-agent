729abe8b8dfbb9c7dc1a0e8994c0d9ea0a884123
jmc <33655003+jmcook1186@users.noreply.github.com>
2025-12-17 14:42:48 +0000

    update agent - exec script and request summary report

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
 from dotenv import load_dotenv
-
 from mistralai import Mistral
-load_dotenv()
+from consensus_metrics import run_pipeline
+
+
 client = Mistral(api_key=os.getenv("MISTRAL_API_KEY"))
+file_path=os.getenv("FILE_PATH")
+
+clusters, reduced_embeddings, lda, sentiments = run_pipeline(file_path)
 
 quant = client.beta.agents.create(
     model="mistral-medium-latest",
@@ -12,17 +19,28 @@ quant = client.beta.agents.create(
     instructions= """
         You are Quant, an advanced AI assistant specialized in data analysis, statistical reporting, and visualization.
         Your primary role is to help users ingest, analyze, and interpret datasets, generate summary statistics, identify trends, and create clear, actionable reports and visualizations.
-        - Always confirm the user’s requirements and dataset details before starting analysis.
-        - Break down complex, multistage analyses into clear steps and explain your approach.
-        - Use best practices for reproducibility and documentation.
-        - Offer to save or export results (code, visualizations, reports) as needed.
-        - Python: Pandas, NumPy, SciPy, StatsModels, Scikit-learn
-        - Visualization: Matplotlib, Seaborn, Plotly, Altair. Prioritise matplotlib.
-        - Reporting: Jupyter Notebooks, Markdown, LaTeX , PDF
-        - Ask clarifying questions to ensure accuracy.
-        - Provide code snippets and explanations side-by-side for transparency.
         """,
+        completion_args={"temperature": 0.2},
+        tools=[{"type": "web-search",
+                "type":"code_interpreter",
+                }],
     )
 
-print(quant.id)
-print(quant.description)
+message = f"""I have run the script consensus_metrics.py and generated the following datasets: 
+ clusters: {clusters},
+ reduced_embeddings: {reduced_embeddings}, 
+ lda: {lda}, 
+ sentiments: {reduced_embeddings}
+ Your task is to analyse those datasets and return a summary report for the state of the conversation as it is represented in those datasets.
+ You may also provide recommendations for additional analyses I might like to run, and reccomendations for what we could do to advance the conversation.
+ Structure your response appropriately so that I can extract the content and save it as a markdown file.
+"""
+
+response = client.beta.conversations.start(
+    agent_id=quant.id,
+    inputs=message,
+    
+)
+
+with open(f"summary_report.md", 'x') as f:
+    f.write(response.outputs[0].content)
